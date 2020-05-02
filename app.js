2585f88e18350e7e6f6ede709b7a3f230a2d4a83
jpark799 <jason.j.park79@gmail.com>
2020-05-02 03:48:06 -0400

    Added checkout route

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// The checkout route
+var checkout = require('./routes/checkout');
+app.use('/checkout', checkout);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
